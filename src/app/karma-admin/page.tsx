eb2d7fe7c7cfa26c79c17d4a8a0e02e657c07889
chiguiredev <rafaelsalass20@gmail.com>
2024-02-22 14:08:28 +0100

    feature: has admin protected karma dashboard

--- src/app/karma-admin/page.tsx
+++ src/app/karma-admin/page.tsx
@@ -1,4 +1,18 @@
-export default function KarmaAdmin() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import type { Session } from "next-auth";
+
+export default async function KarmaAdmin() {
+
+  const session: Session | null = await getServerSession(authOptions);
+
+  console.log(session);
+
+  if (!session || session?.user?.name !== "admin") {
+    redirect("/");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
